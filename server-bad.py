ab1f06d279922bef1b2420f0abc9ca6de3969abc
Sam <samfelt@users.noreply.github.com>
2021-03-23 18:09:30 -0700

    Retry Introducing errors to test flake8 action

--- server-bad.py
+++ server-bad.py
@@ -1,19 +1,15 @@
 from http.server import SimpleHTTPRequestHandler
 from socketserver import TCPServer
 import ssl
-
-
 def main():
     interface = 'localhost'
     port = 8080
-    httpd = TCPServer((interface, port), SimpleHTTPRequestHandler)
+    httpd = TCPServer((interface,port),SimpleHTTPRequestHandler)
     try:
         print(f"Serving HTTP on {interface}, port {port}...")
         httpd.serve_forever()
-    except KeyboardInterrupt:
+    except KeyboardInterrupt as e:
         print("\nRecieved Keyboard Interupt... Shutting Down")
         httpd.server_close()
-
-
 if __name__ == '__main__':
     main()
