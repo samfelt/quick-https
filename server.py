0308ec4400c0a9d5439e571f3524f20d4a323833
Sam <samfelt@users.noreply.github.com>
2021-03-29 21:12:04 -0700

    Added arg_parse with interface and port arguments

--- server.py
+++ server.py
@@ -1,10 +1,36 @@
 from http.server import SimpleHTTPRequestHandler
 from socketserver import TCPServer
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--interface",
+        "-i",
+        type=str,
+        default="0.0.0.0",
+        action="store",
+        help="Network interface to use",
+    )
+    parser.add_argument(
+        "port",
+        nargs="?",
+        type=int,
+        default=8000,
+        action="store",
+        help="Port to liston on",
+    )
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="Print extra info"
+    )
+    return parser.parse_args()
 
 
 def main():
-    interface = "localhost"
-    port = 8080
+    args = parse_args()
+    interface = args.interface
+    port = args.port
     httpd = TCPServer((interface, port), SimpleHTTPRequestHandler)
     try:
         print(f"Serving HTTP on {interface}, port {port}...")
