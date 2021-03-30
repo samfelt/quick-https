6941953fb687e055638b1ad70f50b57a8d20162e
Sam <samfelt@users.noreply.github.com>
2021-03-30 12:57:05 -0700

    Take key and cert from keys directory to create a tls connection

--- server.py
+++ server.py
@@ -1,6 +1,18 @@
+import argparse
+import ssl
 from http.server import SimpleHTTPRequestHandler
 from socketserver import TCPServer
-import argparse
+
+KEY_DIR = "keys"
+CIPHERS = "ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:\
+           ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:\
+           DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:\
+           ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:\
+           ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:\
+           ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:\
+           ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:\
+           DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:\
+           DHE-RSA-AES256-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!3DES:!MD5:!PSK"
 
 
 def parse_args():
@@ -32,8 +44,18 @@ def main():
     interface = args.interface
     port = args.port
     httpd = TCPServer((interface, port), SimpleHTTPRequestHandler)
+    httpd.socket = ssl.wrap_socket(
+        httpd.socket,
+        keyfile=f"{KEY_DIR}/keyfile.key",
+        certfile=f"{KEY_DIR}/certfile.crt",
+        server_side=True,
+        ssl_version=ssl.PROTOCOL_TLSv1_2,
+        ca_certs=None,
+        do_handshake_on_connect=True,
+        suppress_ragged_eofs=True,
+    )
     try:
-        print(f"Serving HTTP on {interface}, port {port}...")
+        print(f"Serving HTTPS on {interface}, port {port}...")
         httpd.serve_forever()
     except KeyboardInterrupt:
         print("\nRecieved Keyboard Interupt... Shutting Down")
